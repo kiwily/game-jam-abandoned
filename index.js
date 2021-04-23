b4c0d6ad15eca5185a4cde6fc19e1a4e4478d7b1
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-24 00:03:21 +0100

    Style and Terrain Base

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const WIDTH = 800;
+const HEIGHT = 800;
+
 // module aliases
 const Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
     Body = Matter.Body,
-    Composite = Matter.Composite,
-    Vector = Matter.Vector;
+    Bodies = Matter.Bodies,
+    Vector = Matter.Vector,
+    Composite = Matter.Composite;
 
 // create an engine
 const engine = Engine.create();
@@ -13,5 +16,12 @@ const engine = Engine.create();
 // create a renderer
 const render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+      width: WIDTH,
+      height: HEIGHT,
+      background: 'black',
+      wireframeBackground: 'white'
+    }
+    ,
 });
--- src/terrain.js
+++ src/terrain.js
@@ -3,7 +3,10 @@
 function Terrain() {
   return (
     [
-      Bodies.rectangle(400, 610, 810, 60, { isStatic: true }),
+      Bodies.rectangle(WIDTH / 2, 0, WIDTH, 50, { isStatic: true }),
+      Bodies.rectangle(WIDTH / 2, HEIGHT, WIDTH, 50, { isStatic: true }),
+      Bodies.rectangle(WIDTH, HEIGHT / 2, 50, HEIGHT, { isStatic: true }),
+      Bodies.rectangle(0, HEIGHT / 2, 50, HEIGHT, { isStatic: true }),
     ]
   );
 };
