93aa64fab1fc4fcc2577961f866f851c8197c03e
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-23 22:54:47 +0100

    Add Terrain

--- src/gameManager.js
+++ src/gameManager.js
@@ -2,14 +2,16 @@
 // let player2Event = "PLAYER_2_EVENT";
 
 function GameManager() {
-  const component = {
+  Composite.add(engine.world, Terrain());
+
+
+  const squares = {
     squareA: Square(400, 200, "bonjour"),
     squareB: Square(450, 50, "bonjour"),
-    ground: Bodies.rectangle(400, 610, 810, 60, { isStatic: true }),
   };
 
   // add all of the bodies to the world
-  Composite.add(engine.world, Object.values(component));
+  Composite.add(engine.world, Object.values(squares));
 };
 
 // const init = () => {
---
+++ src/terrain.js
@@ -0,0 +1,9 @@
+// src/terrain.js
+
+function Terrain() {
+  return (
+    [
+      Bodies.rectangle(400, 610, 810, 60, { isStatic: true }),
+    ]
+  );
+};
