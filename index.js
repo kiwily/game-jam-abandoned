c09d0d129b8211eba3cb3fa76edcb39c0871f578
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-24 13:49:10 +0100

    Host

--- index.js
+++ index.js
@@ -52,7 +52,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 io.on("connection", (socket) => {
