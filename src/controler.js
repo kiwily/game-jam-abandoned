df57d3eb7f79b0dd708eac2840b2283a2449c937
Tanguy Escaron <tanguy.escaron@hotmail.fr>
2021-04-24 04:38:31 +0100

    cleaning controler file

--- src/controler.js
+++ src/controler.js
@@ -16,10 +16,6 @@ const keyDic = {
         "player": 1,
         "direction": "DOWN"
     },
-    // 81: {
-    //     "player": 1,
-    //     "direction": "SWITCH"
-    // },
     38: {
         "player": 2,
         "direction": "UP"
@@ -35,46 +31,34 @@ const keyDic = {
     40: {
         "player": 2,
         "direction": "DOWN"
-    },
-    // 190: {
-    //     "player": 2,
-    //     "direction": "SWITCH"
-    // }
+    }
 }
 const Controler = (player1Event, player2Event) => {
+    const playerEvent = [player1Event, player2Event]
 
+    // Switch commands during a switch event
     window.addEventListener('SWITCH', function (e) {
-        [player1Event, player2Event] = [player2Event, player1Event]
+        playerEvent = [playerEvent[1], playerEvent[0]]
     });
+
+    // Function to fire the right player's movement
     function handleKey(e, triggered){
         const { player, direction } = keyDic[e.keyCode] || [null, null]
-        // console.log(player, direction)
-
-        if (direction === "SWITCH") {
-            window.dispatchEvent(new Event("SWITCH"))
-        } else {
-            if (player === 1) {
-                window.dispatchEvent(new CustomEvent(player1Event, { 
-                    bubbles: true,
-                    detail:{
-                        direction: direction,
-                        triggered
-                    } 
-                }));
-            } else if (player === 2) {
-                window.dispatchEvent(new CustomEvent(player2Event, {
-                    bubbles: true,
-                    detail:{
-                        direction: direction,
-                        triggered
-                    } 
-                }));
-            }
-        }
+        window.dispatchEvent(new CustomEvent(playerEvent[player], { 
+            bubbles: true,
+            detail:{
+                direction: direction,
+                triggered
+            } 
+        }));
     }
+
+    // Dispatch key pressed
     window.addEventListener('keydown', function (e) {
         handleKey(e, true)
     }, false);
+
+    // Dispatch key released
     window.addEventListener('keyup', function (e) {
         handleKey(e, false)
     }, false);
