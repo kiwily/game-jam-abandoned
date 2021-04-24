5512e550779fa0a0d559848bdf17557913c158fe
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-24 12:05:47 +0100

    Change port to 8080

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const port = 3000;
+const port = 8080;
 
 const app = express();
 const server = http.createServer(app);
