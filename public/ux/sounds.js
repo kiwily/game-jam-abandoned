12d7be23d48ae4ee00b1f4a9d21caa66cf028878
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-24 22:59:37 +0100

    Sound Back

--- public/ux/sounds.js
+++ public/ux/sounds.js
@@ -1,8 +1,22 @@
 const btnAudio = document.querySelector("#btn-audio");
 
-const audioGameBackground = new Audio("/assets/Minimalistic Loops/loopThree.wav");
+const audioGameBackground = new Audio("/assets/Minimalistic Loops/loopTwo.wav");
+audioGameBackground.loop = true;
+audioGameBackground.volume = 0.3;
 
-// audioGameBackground.addEventListener("canplaythrough", event => {
-//   /* the audio is now playable; play it if permissions allow */
-//   audioGameBackground.play();
-// });
+const audioCanBePlay = false;
+audioGameBackground.addEventListener("canplaythrough", event => {
+  /* the audio is now playable; play it if permissions allow */
+  btnAudio.disabled = false;
+});
+
+let hasAudio = false;
+btnAudio.addEventListener("click", (e) => {
+  if (hasAudio) {
+    audioGameBackground.pause();
+    hasAudio = false;
+  } else {
+    audioGameBackground.play();
+    hasAudio = true;
+  };
+});
